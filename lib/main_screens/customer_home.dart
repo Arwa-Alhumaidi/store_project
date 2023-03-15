4429de8b3fffcd1a4325fe8dc69eced1fd4a63da
Arwa Alhumaidi <arwa.alhumaidi93@Gmail.com>
2023-03-15 12:23:31 +0300

    first commit

---
+++ lib/main_screens/customer_home.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:store_project/main_screens/home.dart';
+
+class CustomerHome extends StatefulWidget {
+  const CustomerHome({super.key});
+
+  @override
+  State<CustomerHome> createState() => _CustomerHomeState();
+}
+
+class _CustomerHomeState extends State<CustomerHome> {
+  int _selectedIndex = 0;
+  final List<Widget> _tabs = const [
+    Home(),
+    Center(
+      child: Text('category screen'),
+    ),
+    Center(
+      child: Text('stores screen'),
+    ),
+    Center(
+      child: Text('cart screen'),
+    ),
+    Center(
+      child: Text('profile screen'),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        selectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Category',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Stores',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
